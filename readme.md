d052bb34b1fbd0deb0093e63e1608350e1325d91
Leafarsf <rafael.sfernandes97@gmail.com>
2022-07-19 14:35:29 -0300

    aula 2

---
+++ readme.md
@@ -0,0 +1,15 @@
+-<h1> Imersão Java Alura </h1>
+<hr>
+
+Repositório criado para inserção dos desafios e atividades da imersão java Alura
+
+
+<h2> Aula 1 (v1.0) </h2>
+<hr>
+
+Na aula 1, foi realizada a implementação de visualização dos 250 principais filmes da API do IMDB (International Movie Database). Como desafio adicional, inseri também a visualização dos TOP 10 filmes atuais e também o TOP 10 de séries atuais. Foram consumidas duas API's diferentes para a execução deste exercício.
+
+<h2> Aula 2 (v1.2) </h2>
+<hr>
+
+Na aula 2, foi criada um gerador de figurinhas para o Whatsapp explorando bibliotecas nativas do Java. 
---
+++ v1.2/src/App.java
@@ -0,0 +1,46 @@
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // pegar os dados do IMDB, fazer a conexão com a API (http) e salvar os dados em string e pegar os top 250 filmes
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+
+        ///extrair (parsear) só os dados q precisa (titulo, poster, classificação)
+        var Parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = Parser.parse(body);
+
+        // exibir e manipular os dados
+        System.out.println("\u001b[31m Filmes do top 250 geral do IMDB: \u001b[0m");
+
+        for (Map<String,String> filme : listaDeFilmes) {
+            
+            String urlImagem = filme.get("image");
+            String nomeFilme = filme.get("title");
+            
+            InputStream inputStream = new URL(urlImagem).openStream();
+
+            String nomeArquivo = nomeFilme + ".png";
+
+            var gerar = new geradorDeFigurinhas();
+            gerar.cria(inputStream, nomeArquivo);
+            System.out.println("imprimindo..." + filme.get("title"));
+        
+        }    
+        }
+        
+        }
+        
